f419d4d9e2980ff86dee069689caa8015dde60c1
aaa <aaa>
2023-06-16 08:00:14 -0500

    cosa esa del post

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE psychologist;
+
+USE psychologist;
+
+CREATE TABLE patients(
+    id_patient INT PRIMARY KEY AUTO_INCREMENT,
+    edad INT NOT NULL,
+    direccion VARCHAR(60) NOT NULL,
+    proyeccion_laboral VARCHAR(60),
+    estado_civil VARCHAR(60),
+    hobby VARCHAR(20)
+);
+
